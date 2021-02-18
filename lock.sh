4c7521027a96f069e2a405037971c5928cac2173
Rémi Berson <remi@cliqz.com>
2021-02-18 09:13:58 +0100

    Bump

--- lock.sh
+++ lock.sh
@@ -1,20 +1,20 @@
 # https://github.com/nodejs/node/releases/latest
-export NODEJS="15.7.0"
+export NODEJS="15.9.0"
 
 # https://github.com/keepassxreboot/keepassxc/releases/latest
-export KEEPASSXC="2.6.3"
+export KEEPASSXC="2.6.4"
 
 # https://github.com/neovim/neovim/releases/latest
 export NEOVIM="v0.4.4"
 
 # https://github.com/hadolint/hadolint/releases/latest
-export HADOLINT="v1.20.0"
+export HADOLINT="v1.21.0"
 
 # https://github.com/BurntSushi/ripgrep/releases/latest
 export RIPGREP="12.1.1"
 
 # https://github.com/rust-analyzer/rust-analyzer/releases/latest
-export RUST_ANALYZER="3ab8d7a"
+export RUST_ANALYZER="1a59f75"
 
 # https://github.com/mozilla/sccache/releases/latest
 export SCCACHE="0.2.15"
@@ -30,14 +30,14 @@ export VAGRANT="2.2.14"
 
 # https://github.com/hashicorp/terraform/releases/latest
 # https://www.terraform.io/downloads.html
-export TERRAFORM="0.14.5"
+export TERRAFORM="0.14.6"
 
 # https://www.python.org/downloads/
 export PYTHON="3.8.7"
 
 # https://golang.org/dl/
-export GO="1.15.4"
+export GO="1.15.8"
 
 # Glasgow Haskell Compiler
 export HASKELL_GHC="8.10.3"
-export HASKELL_LANGUAGE_SERVER="0.8.0"
+export HASKELL_LANGUAGE_SERVER="0.9.0"
