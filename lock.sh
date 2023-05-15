7e3f68ba866941888b189e06465319e621b2f499
Rémi Berson <remi@brave.com>
2023-05-15 10:29:44 +0200

    Updates

--- lock.sh
+++ lock.sh
@@ -1,8 +1,8 @@
 # https://github.com/nodejs/node/releases/latest
-export NODEJS="19.9.0"
+export NODEJS="20.1.0"
 
 # https://github.com/keepassxreboot/keepassxc/releases/latest
-export KEEPASSXC="2.7.4"
+export KEEPASSXC="2.7.5"
 
 # https://github.com/neovim/neovim/releases/latest
 export NEOVIM="v0.9.0"
@@ -14,13 +14,13 @@ export HADOLINT="2.10.0"
 export RIPGREP="13.0.0"
 
 # https://github.com/rust-analyzer/rust-analyzer/releases/latest
-export RUST_ANALYZER="01120f1"
+export RUST_ANALYZER="e5c7228"
 
 # https://github.com/mozilla/sccache/releases/latest
-export SCCACHE="0.4.1"
+export SCCACHE="0.4.2"
 
 # https://github.com/docker/compose/releases/latest
-export DOCKER_COMPOSE="v2.17.2"
+export DOCKER_COMPOSE="v2.17.3"
 
 # https://www.vagrantup.com/downloads
 export VAGRANT="2.3.4"
@@ -33,7 +33,7 @@ export TERRAFORM="1.1.9"
 export PYTHON_VERSION="3.11.3"
 
 # https://golang.org/dl/
-export GO="1.20.1"
+export GO="1.20.4"
 
 # https://github.com/koalaman/shellcheck/releases/latest
 export SHELLCHECK="0.9.0"
